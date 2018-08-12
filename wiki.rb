fd8dd1bbcdcca871774a15a338dcd8b44911a815
JQuinnie <jenn8866@gmail.com>
2018-08-12 11:24:19 -0600

    Add method to load text files

--- wiki.rb
+++ wiki.rb
@@ -2,8 +2,18 @@ require 'sinatra'
 
 set :bind, '0.0.0.0'
 
+# method to load text files
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
 get '/' do
   # insert erb template of file welcome.erb
   erb :welcome
 end
 
+get '/:title' do
+  page_content(params[:title])
+end
